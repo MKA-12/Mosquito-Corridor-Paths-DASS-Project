00c3d09c98c87ce9cf23b43c340627804826d82d
chinnu_sai25 <shashank0225@gmail.com>
2020-04-21 13:07:57 +0530

    Hashing of monitors password on registration is done

--- src/web-app/routes/verification.js
+++ src/web-app/routes/verification.js
@@ -2,10 +2,13 @@ const express = require('express');
 const MonitorRouter = express.Router();
 const Monitor = require('../models/monitor')
 var crypto = require('crypto');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../config/keys");
 
-MonitorRouter.get('/:id', function (req, res) {
+MonitorRouter.get('/:id', function(req, res) {
     let hashId = req.params.id
-    Monitor.find(function (err, monitors) {
+    Monitor.find(function(err, monitors) {
         if (err) {
             console.log(err);
             res.status(400).send('Error');
@@ -20,37 +23,42 @@ MonitorRouter.get('/:id', function (req, res) {
         }
     });
 });
-MonitorRouter.post('/', function (req, res) {
+MonitorRouter.post('/', function(req, res) {
     let hashId = req.body.id
-    Monitor.find(async function (err, monitors) {
-        if (err) {
-            console.log(err);
-            res.status(400).send('Error');
-        } else {
-            for (var i; i < monitors.length; i++) {
-                if (monitors[i].username === req.body.username) {
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(req.body.password, salt, (err, hash) => {
+            if (err) throw err;
+            req.body.password = hash;
+            Monitor.find(async function(err, monitors) {
+                if (err) {
+                    console.log(err);
+                    res.status(400).send('Error');
+                } else {
+                    for (var i; i < monitors.length; i++) {
+                        if (monitors[i].username === req.body.username) {
+                            res.status(200).send(false)
+                        }
+                    }
+                    for (var i = 0; i < monitors.length; i++) {
+                        if (hashId == crypto.createHash('md5').update((monitors[i]._id).toString()).digest('hex') && monitors[i].verified == false) {
+                            await Monitor.findByIdAndUpdate(monitors[i]._id, { "username": req.body.username, "password": hash, "verified": true },
+                                function(err, response) {
+                                    if (err) {
+                                        res.status(400).send('Error');
+                                        console.log(err)
+                                    } else {
+                                        res.status(200).send(true);
+                                    }
+                                })
+                            return;
+                        }
+                    }
                     res.status(200).send(false)
                 }
-            }
-            for (var i = 0; i < monitors.length; i++) {
-                if (hashId == crypto.createHash('md5').update((monitors[i]._id).toString()).digest('hex') && monitors[i].verified == false) {
-                    await Monitor.findByIdAndUpdate(monitors[i]._id, { "username": req.body.username, "password": req.body.password, "verified": true },
-                        function (err, response) {
-                            if (err) {
-                                res.status(400).send('Error');
-                                console.log(err)
-                            }
-                            else {
-                                res.status(200).send(true);
-                            }
-                        })
-                    return;
-                }
-            }
-            res.status(200).send(false)
-        }
-    }
-    );
+            });
+        });
+    });
+
 
 
 })
